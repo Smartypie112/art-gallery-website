5d34f96ade141b981c9a1498879279018d5ac633
Smartypie112 <awmk222@gmail.com>
2026-03-07 17:01:57 +0530

    Vconsole removed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import VConsole from "vconsole";
 
-const vConsole = new VConsole();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
